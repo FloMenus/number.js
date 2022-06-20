4ab6b40399169a12c41afbf073e99df31f52f53e
FloMenus <florent.mns@gmail.com>
2022-06-20 16:54:29 +0200

    .

---
+++ index.js
@@ -0,0 +1,92 @@
+// 1 - Number
+
+// const integer = 102;
+// const float = 13.9;
+
+// console.log (integer , float)
+
+// 2 - Convert
+
+// const basic = 34;
+// const stringified = basic.toString();
+
+// console.log (stringified)
+
+// 3 - Round
+
+// const num = 1.5;
+// const rounded = Math.round(num);
+
+// console.log (rounded)
+
+// 4 - Arithmétique
+
+// const test = 12;
+// const bis = 5;
+
+// console.log (test + bis)
+// console.log (test - bis)
+// console.log (test / bis)
+// console.log (test * bis)
+// console.log (test % bis)
+
+// 5 - Reste divison euclidienne 
+
+
+
+
+
+// let modulo = 999 %1; // Oui
+// console.log(modulo);
+// let modulo = 999 %2 // non 
+// console.log(modulo); 
+// let modulo = 999 %3 // oui
+// console.log(modulo); 
+// let modulo = 999 %4// oui
+// console.log(modulo); 
+// let modulo = 999 %5
+// console.log(modulo); //non
+
+// let modulo = 25 %1; //oui 
+// console.log(modulo);
+// let modulo = 25 %2
+// console.log(modulo); // non
+// let modulo = 25 %3
+// console.log(modulo); // non 
+// let modulo = 25 % 4
+// console.log(modulo); // non
+// let modulo = 25 %5
+// console.log(modulo); // oui 
+
+// let modulo = 190280 %1; 
+// console.log(modulo); // oui
+// let modulo = 190280  %2
+// console.log(modulo);  // oui
+// let modulo = 190280  %3
+// console.log(modulo); // non
+// let modulo = 190280  % 4
+// console.log(modulo); // oui 
+// let modulo = 190280  %5
+// console.log(modulo); // oui
+
+// let modulo = 47%1; // oui 
+// console.log(modulo);
+// let modulo = 47 %2
+// console.log(modulo); // non 
+// let modulo = 47 %3
+// console.log(modulo); // non 
+// let modulo = 47 % 4
+// console.log(modulo); // non 
+// let modulo = 47 %5
+// console.log(modulo); // non 
+
+// let modulo = 691%1;
+// console.log(modulo); // oui
+// let modulo = 691 %2
+// console.log(modulo);// non
+// let modulo = 691 %3
+// console.log(modulo);// non
+// let modulo = 691 % 4
+// console.log(modulo); // non
+// let modulo = 691 %5
+// console.log(modulo); // non
